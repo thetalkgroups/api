47ca7b46e29b5912332f41cc64d28d432631c719
Mathias Hansen <mathias.hansen783@gmail.com>
2016-10-22 16:24:59 +0200

    changed cors to dev and prod

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,18 @@ new MongoClient().connect("mongodb://db:27017/ttg").then(async db => {
 
     const app = express();
 
-    app.use(cors({ origin: "http://localhost:4000" }));
+    app.use((req: Request, res: express.Response, next: () => void) => {
+        const origin = req.headers["origin"];
+        
+        if (origin === "http://localhost:4000") {
+            res.setHeader("allow-control-allow-origin", "http://localhost:4000");
+        }
+        else if (origin === "https://thetalkgroups.github.io") {
+            res.setHeader("allow-control-allow-origin", "https://thetalkgroups.github.io");
+        }
+
+        next();
+    });
 
     app.use(wrap(async (req, res, next) => {
         const userId = req.header("Authorization");
